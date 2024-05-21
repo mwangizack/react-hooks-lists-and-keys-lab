439ee0af22f721a3dcbac380f860d2402798510f
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-21 03:42:36 +0300

    Render arrays of components

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((projectObj) => (
+          <ProjectItem
+            key={projectObj.id}
+            name={projectObj.name}
+            about={projectObj.about}
+            technologies={projectObj.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
